f8b9d744c1a7002735bc540667da8a1f7e791c80
USER <USER@DESKTOP-ISNR6FU>
2024-10-09 16:49:09 +0900

    <수정사항> 테스트입니다.

--- 소스.cpp
+++ 소스.cpp
@@ -9,35 +9,31 @@
 //--------------------------------------------------------------------------------------
 
 #include <iostream>
+#include <chrono>
+#include <thread>
 #include <random>
 #include <print>
 #include <array>
 
+
 #include "save.h"
 
+using namespace std::literals;
 
 int main()
 {
-	//[문제] 사용자가 입력한 숫자만큼 int를 저장할 메모리를 확보하라 
-	//이 메모리를 1부터 시작하는 정수로 채워라
-	//메모리에 저장된 모든 정수의 합을 화면에 출력하라
-
-	int in{-1}, sum{0};
-	std::cin >> in;
-
-	int* arr = (int*)calloc(in, sizeof(int));//malloc ( contiguous 연속적인 메모리를 요청)
-	if (arr == NULL) { return 1; }
-	for (int i = 0; i < in; ++i) {
-		arr[i] = i+1;
-		sum += i+1;
+	int cnt{};
+	//메모리를 계속 요청한다면?
+	while (true) {
+		//메모리 요청은 실패할 수 있다.
+		//실패 시 메모리 관리자는 예외를 던진다. 
+		//이 예외를 처리 하니 않으면 프로그램은 비정상 종료한다. 
+		char* c = new char[2'000'000'000];
+		std::cout << ++cnt << "-메모리 할당 성공" << std::endl;
+		
+		std::this_thread::sleep_for(0.5s);
+		delete(c);
 	}
-
-	std::cout << sum << std::endl;
-
-	free(arr);
-
-	//const int num{333};
-	//int n[num]; const 사용하면 가능 , 그렇지 않으면 가변적이라 불가능
 
 	save("소스.cpp");
 }
