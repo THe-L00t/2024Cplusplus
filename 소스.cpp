5068db1c60814f16ad76a3d53bb32898fb09c8d6
USER <USER@DESKTOP-ISNR6FU>
2024-10-16 16:45:20 +0900

    개인 문제 풀이

--- 소스.cpp
+++ 소스.cpp
@@ -17,9 +17,9 @@
 int main()
 {
 	//[문제] 파일 "랜덤값 몇개.mp3"에는 몇 개인지 모르는 int 값이
-	//저장되어 있다. 
-	// 모두 몇 개인지 출력하라
-	//가장 큰 값을 찾아 화면에 출력하라. 
+	//저장되어 있다.
+	// 오름차순 정렬하려 화면 출력하라
+	// 
 
 	std::ifstream in{ "랜덤값 몇개.mp3" };
 	if (not in) {		// 에러 확인 필수 
@@ -28,13 +28,19 @@ int main()
 	}
 
 	int count{ 0 }, max{ -1 }, num;
+	int i{ 0 };
 	while (in>>num) {	//!in.eof() 사용하지 않기  : in >> num 사용하기 
-	/*	int num;
-		in >> num;*/
 
 		++count;
 		max = (max < num) ? num : max;
 	}
+	int* arr = new int[count];
+	in.seekg(0);
+	while (in >> num) {
+		arr[i] = num;
+		++i;
+	}
+	qsort(arr, count, 4, );
 
 	std::cout << "int의 개수 : " << count << std::endl << "가장 큰 값 : " << max << std::endl;
 
