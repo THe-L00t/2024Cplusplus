16b3c558c6a7dc0bb9f68d08a4e8238b1b3f0d97
USER <USER@DESKTOP-ISNR6FU>
2024-10-17 17:01:21 +0900

    중간수정

--- 소스.cpp
+++ 소스.cpp
@@ -13,30 +13,13 @@
 
 #include "save.h"
 
-//struct와 class를 사용하여 새 자료형을 만든다. 
-class person {
-	std::string name{ "응애" };
-private:		//access modifier - class default
-
-}typedef Person;
 struct Dog {
-public:			//struct default
-	std::string name{ "응애" };
-	int age;
-	int kind;
-	char color;
-
-	void bark();
-	void run();
-}typedef Dog;
-
+	char c;
+	int a;
+};
 int main()
 {	
-	Dog p;
-	std::cout << p.name << std::endl;
-	p.name = "응애요";	//바꾸지 못하게 하려면 클래스로 선언
-	std::cout << p.name << std::endl;
-	Dog d;
+	Dog dog;
 	(*save)("소스.cpp");//정석 함수 호출
 }
 
