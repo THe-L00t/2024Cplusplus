7c9c0b0e50b215beb95c540abd8ceb24393f93f4
USER <USER@DESKTOP-ISNR6FU>
2024-11-20 17:19:48 +0900

    교수님 수정버전

--- 소스.cpp
+++ 소스.cpp
@@ -34,10 +34,10 @@ public:
 		++n;
 		return *this;
 	}
-	INT& operator++(int i) {
-		INT temp{*this};
-		n += 1;
-		return temp;
+	INT operator++(int i) {
+		INT temp{*this};	//현재의 나를 저장한다.
+		++(*this);			//나를 1 증가시킨다. 
+		return temp;		//저장했던 나를 리턴한다. 
 	}
 	friend std::ostream& operator<<(std::ostream& os, const INT& n);
 	
