873088ede0b70cc0700b24fcc8b3609b562dd12f
USER <USER@DESKTOP-ISNR6FU>
2024-11-21 15:36:29 +0900

    후위증감연산자의 빨간줄 만들기 + 주석에 적어둠

--- 소스.cpp
+++ 소스.cpp
@@ -34,7 +34,7 @@ public:
 		++n;
 		return *this;
 	}
-	INT operator++(int i) {
+	/*const*/INT operator++(int i) {
 		INT temp{*this};	//현재의 나를 저장한다.
 		++(*this);			//나를 1 증가시킨다. 
 		return std::move(temp);		//저장했던 나를 리턴한다. 
