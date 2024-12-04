ff3216a8402704f6e24e3048450715a274f619ec
USER <USER@DESKTOP-ISNR6FU>
2024-12-04 16:29:03 +0900

    설명 plus

--- 소스.cpp
+++ 소스.cpp
@@ -22,9 +22,10 @@
 
 class Animal {
 public:
-	virtual void move() const {
+	virtual void move() const {	//virtual을 사용하면 virtual pointer가 추가 할당 된다. 
 		std::cout << "동물의 move" << std::endl;
 	}
+	//포인터이므로 8바이트 
 private:
 	int a;
 };
@@ -46,7 +47,7 @@ public:
 private:
 	//char c; 8bite = 4bite,1bite,3bite(data padding)
 	int b;
-};
+}; //상속 받을때에는 부모 클래스의 메모리는 건드리지 않는다. 
 
 int main()
 {
@@ -54,9 +55,20 @@ int main()
 	Dog dog;
 	Bird bird;
 
-	animal.move();
-	dog.move();
-	bird.move();
+	//polymorphism 을 구현하려면 객체를 부모의 *로 가리켜야 한다. 
+	Animal* animals[3];
+	animals[0] = &animal;
+	animals[1] = &dog;
+	animals[2] = &bird;
+
+	//모든 동물들에게 move()를 호출한다. 
+	for (int i = 0; i < 3; i++)
+	{
+		animals[i]->move();		//이 함수는 dynamic binding
+	}
+
+	//다형성은 메모리를 막대하게 희생하여 유연함을 얻는다. 
+
 	//같은 이름의 함수라면 c++에서는 name mangling하기 때문에 구분 가능하다. 
 	// 추가 기호를 붙여 구분한다. 
 
