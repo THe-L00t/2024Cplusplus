7637d4299fab2dd9896103e4027a00d07ae4920c
USER <USER@DESKTOP-ISNR6FU>
2024-12-11 17:00:05 +0900

    추가 설명

--- 소스.cpp
+++ 소스.cpp
@@ -16,43 +16,37 @@
 //--------------------------------------------------------------------------------------
 
 #include <iostream>
-#include <algorithm>
 
 #include "save.h"
 
-template<class X>
+template<class X, int N>
 class SafeArray {
 public:
-	SafeArray(X n) : num{ n } {
-		np = new X[num];
-	}
-	~SafeArray() {
-		delete[] np;
-	}
+	//생성시 아무것도 하지 않으면 rule of zero
 	//복사 생성자 / 복사 할당 연산자 
 	//클래스에 동적할당이 이용되었다면 rule of three
 	//소멸자 복사 생성자 복사 할당 연산자 세 가지는 무조건 코딩하기
 
 	//modern c++ 에서는 이동 생성자, 이동 할당 연산자까지 rule of five
 	int operator[](int idx) {
-		if ( idx < 0 || idx >= num) throw idx;
-		return np[idx];
+		if ( idx < 0 || idx >= N) throw idx;
+		return dat[idx];
 	}
-	int size() const
+	/*constexpr*/ int size() const //3학년 내용
 	{
-		return num;
+		return N;
 	}
 private:
-	X num;
-	int* np;
+	X dat[N];
 };
 
 int main() 
 {
 	// [문제] 의도대로 실행되게 하라 
 
-	SafeArray<int> a{ 10 };		//int 10개를 저장할 배열을 생성한다. 
+	SafeArray<int, 10> a;		//int 10개를 저장할 배열을 생성한다. 
 	//클래스 템플릿은 형식을 꼭 지정해주어야 한다. <int>
+	//배열만 <int, 10> 가능
 
 	for (int i = 0; i < a.size(); ++i)
 	{
